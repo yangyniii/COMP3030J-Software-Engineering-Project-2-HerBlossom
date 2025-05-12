e406bd66636cbde2580aafb1075363ae78c1a144
boranduan <boran.duan@ucdconnect.ie>
2025-05-12 17:12:14 +0800

    change avatar

--- code/hello.py
+++ code/hello.py
@@ -242,7 +242,7 @@ def get_user_info_by_id():
 
 
     # 在确认 user_info 不为空后再访问 avatar
-    avatar_path = user_info.get('avatar', 'chiikawa.jpg')
+    avatar_path = user_info.get('avatar', '../static/images/chiikawa.jpg')
     if not avatar_path.startswith('../'):
         avatar_path = '../static/images/' + avatar_path
     print('Avatar path11:', avatar_path)
